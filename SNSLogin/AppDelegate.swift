2f0645da8c32280abd4ae28b469b399e1d891128
HyeonJeong <dlaus1214@gmail.com>
2024-08-19 23:22:13 +0900

    카카오 로그인 완료

--- SNSLogin/AppDelegate.swift
+++ SNSLogin/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import KakaoSDKAuth
+import KakaoSDKCommon
+import KakaoSDKUser
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDK.initSDK(appKey: "dbe82ca8e4341736f7310a50617b414a")
         return true
     }
 
@@ -29,6 +33,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        return false
     }
 
 
--- SNSLogin/ViewController.swift
+++ SNSLogin/ViewController.swift
@@ -6,9 +6,34 @@
 //
 
 import UIKit
+import KakaoSDKUser
+import KakaoSDKAuth
+import KakaoSDKCommon
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nicknameLabel: UILabel!
+  
+    @IBAction func kakaoLoginButton(_ sender: Any) {
+        // 카카오톡 설치 여부 확인
+        print("클릭")
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+
+                    //do something
+                    _ = oauthToken
+                    self.nicknameLabel.text = "로그인 완료"
+                }
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
