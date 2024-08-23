2d2ae4e2566ab5b82e5b0e83ddee1e448444775f
HyeonJeong <dlaus1214@gmail.com>
2024-08-23 18:52:46 +0900

    구글 로그인 설정

--- SNSLogin/AppDelegate.swift
+++ SNSLogin/AppDelegate.swift
@@ -9,26 +9,28 @@ import UIKit
 import KakaoSDKAuth
 import KakaoSDKCommon
 import KakaoSDKUser
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         KakaoSDK.initSDK(appKey: "dbe82ca8e4341736f7310a50617b414a")
+//        GIDSignIn.sharedInstance.clientID = "485271827723-2nim5arjg4c058v0irutkgnnav2j7kpn.apps.googleusercontent.com"
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
@@ -36,12 +38,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        // kakaoTalk 로그인
         if (AuthApi.isKakaoTalkLoginUrl(url)) {
             return AuthController.handleOpenUrl(url: url)
         }
+        
+        //Goolgle 로그인
+        if GIDSignIn.sharedInstance.handle(url) {
+            return true
+        }
+        
         return false
     }
-
-
+    
+    
 }
 
